4fe5813c0adc3bb53f4364894ccc3d2502b1a5fa
KozlowskiKamil <kamil2k8@gmail.com>
2023-06-01 23:53:18 +0200

    refaktor

---
+++ src/main/java/pl/isa/Palin.java
@@ -0,0 +1,18 @@
+package pl.isa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Palin {
+
+    public static List<String> getPalindrome(String text) {
+        List<String> list = new ArrayList<>();
+        StringBuilder reversed = new StringBuilder(text);
+        if (text.contentEquals(reversed.reverse())){
+            list.add();
+        }
+        return new ArrayList<>();
+    }
+
+
+}
---
+++ src/test/java/pl/isa/PalinTest.java
@@ -0,0 +1,25 @@
+package pl.isa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+public class PalinTest {
+
+    @Test
+    public void getPalindroms_emptyString_emptyList() {
+        assertThat(Palin.getPalindrome("")).hasSize(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"anna", "level", "madam", "kajak"})
+    public void getPalindroms_singlePalindrom_returnsListWithTheSameWorld(String text) {
+        assertThat(Palin.getPalindrome(text)).hasSize(1)
+                .contains(text);
+    }
+    @Test
+    public void getPalindroms_emptyString_emptyList(String text) {
+        assertThat(Palin.getPalindrome(text)).hasSize(0);
+    }
+}
